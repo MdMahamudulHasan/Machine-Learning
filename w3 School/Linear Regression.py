2e434d65e83736f978944f576090d4c8697fbc89
Md Mahamudul Hasan <126501007+MdMahamudulHasan@users.noreply.github.com>
2024-10-18 23:58:50 +0600

    Initial commit

---
+++ w3 School/Linear Regression.py
@@ -0,0 +1,8 @@
+"""
+
+    Regression
+The term regression is used when you try to find the relationship between variables.
+
+In Machine Learning, and in statistical modeling, that relationship is used to predict the outcome of future events.
+
+"""

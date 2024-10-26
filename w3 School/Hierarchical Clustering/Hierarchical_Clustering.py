e3e80b54d69ee89a23db38172cfac43424f5ab69
Md Mahamudul Hasan <126501007+MdMahamudulHasan@users.noreply.github.com>
2024-10-27 00:28:51 +0600

    Initial commit

--- w3 School/Hierarchical Clustering/Hierarchical_Clustering.py
+++ w3 School/Hierarchical Clustering/Hierarchical_Clustering.py
@@ -29,7 +29,52 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 x = [4, 5, 10, 4, 3, 11, 14, 6, 10, 12]
-y = [21, 19, 24, 17, 16, 25, 14, 22, 21, 21]
+y = [21, 19, 24, 17, 16, 25, 24, 22, 21, 21]
 
 plt.scatter(x, y)
+plt.show()
+
+
+
+#Now we compute the ward linkage using euclidean distance, and visualize it using a dendrogram:
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.cluster.hierarchy import dendrogram, linkage
+
+x = [4, 5, 10, 4, 3, 11, 14, 6, 10, 12]
+y = [21, 19, 24, 17, 16, 25, 24, 22, 21, 21]
+
+
+data = list(zip(x, y))
+
+linkage_data = linkage(data, method='ward', metric='euclidean')
+dendrogram(linkage_data)
+
+plt.show()
+
+
+
+
+#Here, we do the same thing with Python's scikit-learn library. Then, visualize on a 2-dimensional plot:
+
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.cluster import AgglomerativeClustering
+
+x = [4, 5, 10, 4, 3, 11, 14, 6, 10, 12]
+y = [21, 19, 24, 17, 16, 25, 24, 22, 21, 21]
+
+
+data = list(zip(x, y))
+
+# Correctly initializing AgglomerativeClustering without affinity
+hierarchical_cluster = AgglomerativeClustering(n_clusters=2, linkage='ward')
+labels = hierarchical_cluster.fit_predict(data) 
+
+# Now you can plot
+plt.scatter(x, y, c=labels)
+plt.title('Hierarchical Clustering')
+plt.xlabel('X-axis')
+plt.ylabel('Y-axis')
 plt.show()
